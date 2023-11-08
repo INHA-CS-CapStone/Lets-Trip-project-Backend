43e2435d1e6be30fb2e01fc229b0a3e18b7f002f
sng123 <101544691+sng123@users.noreply.github.com>
2023-11-08 20:35:31 +0900

    Add files via upload

---
+++ letstrip_server/home/models.py
@@ -0,0 +1,10 @@
+from django.db import models
+from django.core.validators import MinValueValidator, MaxValueValidator
+
+class Place(models.Model):
+    name = models.CharField(max_length=100, blank=True, null=True)
+    keyword = models.CharField(max_length=100, blank=True, null=True)
+    rating = models.FloatField(validators=[MinValueValidator(
+        0.5), MaxValueValidator(5)], blank=True, null=True)
+    type = models.CharField(max_length=100, blank=True, null=True)
+    review_count = models.IntegerField(blank=True, null=True)
---
+++ letstrip_server/process.py
@@ -0,0 +1,16 @@
+import pandas as pd
+import sqlite3
+
+df = pd.read_csv("Place.csv", encoding='cp949')
+
+database = "db.sqlite3"
+conn = sqlite3.connect(database)
+dtype={
+    "name": "CharField",
+    "keyword": "CharField",
+    "rating": "FloatField", 
+    "type": "CharField",
+    "review_count": "IntegerField"
+}
+df.to_sql(name='home_place', con=conn, if_exists='replace', dtype=dtype, index=True, index_label="id")
+conn.close()

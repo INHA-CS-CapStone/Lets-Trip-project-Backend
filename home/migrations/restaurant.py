004a891820a6ce37e9cfdb645a8679df3cac3626
easy-code1 <84382175+easy-code1@users.noreply.github.com>
2023-11-15 19:52:24 +0900

    feat: restaurant 함수 추가

---
+++ home/migrations/restaurant.py
@@ -0,0 +1,28 @@
+import requests
+from bs4 import BeautifulSoup
+
+def restaurant(x, y):
+    name = []
+    url = "http://apis.data.go.kr/B551011/KorService1/locationBasedList1"
+    paramDict = {
+        "MobileOS": "ETC",
+        "MobileApp": "AppTest",
+        "serviceKey": "KWyscPcYr1noVXPOVDIQaqTO/jx61sgGZhTagP0jJQyIWQhZaWLY/wzdUpgKkZ+DB83gnI+Dd7OgXJm3NQHDIg==",
+        "mapX": x,
+        "mapY": y,
+        "radius": 10000,
+        "numOfRows": 1000,
+        "pageNo": 1,
+        "arrange": "E",
+        "listYN": "Y",
+        "contentTypeId": 39
+    }
+
+    res = requests.get(url, params=paramDict)
+    xml = res.text
+    soup = BeautifulSoup(xml, 'html.parser')
+    for tag in soup.find_all('title'):
+        name.append(tag.text)
+
+    return name
+
